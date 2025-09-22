f5fa7537e99c24c8eccddd5be9d9a55908879034
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:25:28 +0500

    Create #Calculate Body Mass Index (BMI).py

---
+++ Assignment 1/#Calculate Body Mass Index (BMI).py
@@ -0,0 +1,12 @@
+#Calculate Body Mass Index (BMI)
+
+weight = int(input("enter the weight=:"))
+
+height= int(input("input height=:"))
+
+MBI = weight / (height ** 2)
+
+print("MBI IS=:",MBI)
+
+print("END HERE" )
+
