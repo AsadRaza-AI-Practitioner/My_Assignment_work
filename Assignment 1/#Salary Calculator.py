6c58e0fd8f143d876080c229f10bee8426b972e1
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:28:04 +0500

    Create #Salary Calculator.py

---
+++ Assignment 1/#Salary Calculator.py
@@ -0,0 +1,15 @@
+#Salary Calculator
+
+Basic_Salary = int(input("imran basic salary is = "))
+
+HRA = (20/100)*Basic_Salary
+
+DA = (15/100)*Basic_Salary
+
+Total_salary = Basic_Salary+HRA+DA
+
+print("Total salary of imran all HRA ABD DA allouance is= ",Total_salary)
+
+print("end of program")
+
+
