05582f2a9ef959c2411e6302439cc13b42f356e9
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:28:15 +0500

    Create #Square and Cube of a Number - Ask the u.py

---
+++ Assignment 1/#Square and Cube of a Number - Ask the u.py
@@ -0,0 +1,17 @@
+#Square and Cube of a Number - Ask the user for a number and display its square and cube.
+
+Numb_1= int(input('enter any number  '))
+
+sq_of_Numb_1 = Numb_1**2
+
+cube_of_Numb_1 = Numb_1**3
+
+print("square of numb_1 is =" ,(sq_of_Numb_1))
+
+print("cube of numb_1 is =" ,(cube_of_Numb_1))
+
+print(type(sq_of_Numb_1))
+
+print(type(cube_of_Numb_1))
+
+print("end of program")
