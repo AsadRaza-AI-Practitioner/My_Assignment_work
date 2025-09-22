5ee5266700db4ccbd12d6ce59097c8b2a81510bd
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:29:58 +0500

    Create perimeter of rectangle.py

---
+++ Assignment 1/perimeter of rectangle.py
@@ -0,0 +1,12 @@
+#Perimeter of a Rectangle - Take length and width as input and calculate the perimeter
+
+length= 19
+width= 12
+
+perimeter =2*(length+width)
+
+print("perimeter of rectangle is = " ,(perimeter))
+
+print(type(perimeter))
+
+print("end program")
