1828ccc4d5abe54ef9db994e3cd6307c02edc262
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:27:04 +0500

    Create #Convert Minutes to Hours and Minutes.py

---
+++ Assignment 1/#Convert Minutes to Hours and Minutes.py
@@ -0,0 +1,9 @@
+#Convert Minutes to Hours and Minutes
+
+min= int(input("enter minutes=:"))
+
+hr= min//60
+
+rem_min = min % 60
+
+print(f"hour is equal to: {hr} and remaing min is equal to:",rem_min)
