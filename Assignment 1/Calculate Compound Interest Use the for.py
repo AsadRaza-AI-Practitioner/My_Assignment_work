2f977d92f42fa7623240d751ee494c3ba1cc659f
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:29:33 +0500

    Create Calculate Compound Interest Use the for.py

---
+++ Assignment 1/Calculate Compound Interest Use the for.py
@@ -0,0 +1,29 @@
+#Calculate Compound Interest Use the formula: CI = P * (1 + R/100)**T - P Where P = principal, R = rate, T = time
+
+P = int(100)
+
+print ("principal is = " ,P)
+
+print(type(P))
+
+R = int(5)
+
+print ("interest rate  is = " ,R)
+
+print(type(R))
+
+T = int(10)
+
+print ("time is = " ,T)
+
+print(type(T))
+
+CI = P * (1 + R/100)**T - P
+
+print("Class interst is = " ,CI)
+
+print(type(CI))
+
+print("END OF PROGRAM")
+
+
