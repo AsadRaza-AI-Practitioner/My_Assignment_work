44da7a526b1bd0f4068851b3b8bc11591ab25b50
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:28:51 +0500

    Create #Write a program that converts a tempera.py

---
+++ Assignment 1/#Write a program that converts a tempera.py
@@ -0,0 +1,12 @@
+#Write a program that converts a temperature from Celsius to Fahrenheit. (Formula: Fahrenheit = (Celsius * 9/5) + 32)
+
+celsius =float(25)
+
+Fahrenheit = ((celsius * 9/5) + 32)
+
+print("temerature in Fahrenheit is :", Fahrenheit )
+
+print(type(Fahrenheit))
+
+print('end program')
+
