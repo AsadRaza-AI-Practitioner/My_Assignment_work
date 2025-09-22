ff90c2bd6494439d52debe5ec8651e8b1bc091cd
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:25:49 +0500

    Create #Age in Months and Days.py

---
+++ Assignment 1/#Age in Months and Days.py
@@ -0,0 +1,13 @@
+#Age in Months and Days
+
+age = int(input("Please enter your age : "))
+
+age_in_month = age*365
+
+age_in_days = age*365*12
+
+print("your age in month is =:" , age_in_month)
+
+print("your age in days is=:" ,age_in_days)
+
+print("end here")
