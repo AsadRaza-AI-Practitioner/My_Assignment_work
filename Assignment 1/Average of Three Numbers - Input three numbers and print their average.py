86ef6f501df067327bfa96535684604543c210b6
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:29:25 +0500

    Create Average of Three Numbers - Input three numbers and print their average.py

---
+++ Assignment 1/Average of Three Numbers - Input three numbers and print their average.py
@@ -0,0 +1,13 @@
+#Average of Three Numbers - Input three numbers and print their average.
+
+numb_1 =int(input('enter 1st number:'))
+
+numb_2 =int(input('enter 2nd number:'))
+
+numb_3 =int(input('enter 3rd number:'))
+
+Avg_number = ((numb_1+numb_2+numb_3) /3)
+
+print('Avg_number is =', Avg_number)
+
+print(type(Avg_number))
