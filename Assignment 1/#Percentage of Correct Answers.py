1f3532202b69101bbc5289d209f0065e629ca97b
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:27:46 +0500

    Create #Percentage of Correct Answers.py

---
+++ Assignment 1/#Percentage of Correct Answers.py
@@ -0,0 +1,11 @@
+#Percentage of Correct Answers
+
+Total_questions = int(input("Total Asked questions"))
+
+correct_answers = int(input("answer which were correct = :"))
+
+percentage_score = (correct_answers/Total_questions)*100
+
+print(f"percentage score is =:,{percentage_score}%")
+
+print("End Here")
