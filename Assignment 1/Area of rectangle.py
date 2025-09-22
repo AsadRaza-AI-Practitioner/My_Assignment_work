63c5bba9ef693d6bc169cc4f5c5106ec95a2e4e4
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 11:29:05 +0500

    Create Area of rectangle.py

---
+++ Assignment 1/Area of rectangle.py
@@ -0,0 +1,17 @@
+
+length= 36
+width= float(29.5)
+
+area =(length*width)
+
+print("Area ofrectangle is = " ,(area))
+
+print(type(area))
+
+print("end program")
+
+score = 18
+total_questions = 20
+result_percentage = (score/total_questions)*100
+
+print(f"Your score percentage is: {result_percentage}%")
