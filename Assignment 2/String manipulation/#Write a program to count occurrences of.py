5930f9d15b763c1a4d9b0b7262e9e29bea9412cd
Asadraza2540 <asadraza2540@gmail.com>
2025-09-23 17:03:12 +0500

    Create #Write a program to count occurrences of.py

---
+++ Assignment 2/String manipulation/#Write a program to count occurrences of.py
@@ -0,0 +1,11 @@
+#Write a program to count occurrences of all characters within a string Given.
+
+str= "Ilovepakistan"
+
+char_dict= dict()
+
+
+for char in str:
+
+    count= str.count(str)
+    print(count)
